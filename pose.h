4aaa9ea4cf01466468424bbef1fffd2e10c61d76
Your Name <you@example.com>
2016-06-03 10:03:19 -0300

    Códigos feitos até agora

---
+++ pose.h
@@ -0,0 +1,99 @@
+#include "util.h"
+#ifndef POSE_H
+#define POSE_H
+#define MOTOR_DIREITO OUT_A
+#define MOTOR_ESQUERDO OUT_B
+#define RAIO_RODA 1.51  //2.7
+#define RAIO_ROBO 7.0  //6.275
+#define CONSTANTE_RODAS (PI * RAIO_RODA)/180
+
+
+struct Pose{
+    float x;
+    float y;
+    float theta;
+};
+
+struct Angulo{
+    int atual;
+    int anterior;
+};
+float distancia_ant;
+float pose_dist = 0;
+float alpha_dir = 0; //ÂNGULO DA RODA DIREITA
+float alpha_esq = 0; //ÂNGULO DA RODA ESQUERDA
+float giro_dir = 0;  //RODA DIREITA
+float giro_esq = 0;  //RODA ESQUERDA
+float inc_angular;   //INCREMENTO ANGULAR
+float s = 0;         //SENO
+float c = 0;         //COSSENO
+Angulo ang_dir,ang_esq;
+Pose dist_perc;
+
+int contador_dir(){
+    ang_dir.anterior = ang_dir.atual;
+    ang_dir.atual = MotorRotationCount(MOTOR_DIREITO);
+    //ResetTachoCount(MOTOR_DIREITO);
+    //ResetRotationCount(MOTOR_DIREITO);
+    return(ang_dir.atual - ang_dir.anterior);
+}
+
+int contador_esq(){
+    ang_esq.anterior = ang_esq.atual;
+    ang_esq.atual = MotorRotationCount(MOTOR_ESQUERDO);
+    return(ang_esq.atual - ang_esq.anterior);
+}
+
+void atualiza_pose(float &x, float &y, float &theta ){
+    float inc_linear;
+    alpha_dir = contador_dir();
+    alpha_esq = contador_esq();
+    giro_dir = alpha_dir * CONSTANTE_RODAS;
+    giro_esq = alpha_esq * CONSTANTE_RODAS;
+    inc_linear = (giro_dir + giro_esq)/2;
+    inc_angular = (giro_dir - giro_esq)/2;
+	pose_dist = inc_linear;	
+	
+    s = Sin(theta);
+    c = Cos(theta);
+    y = y + inc_linear *  s / 100.00;
+    x = x + inc_linear * c / 100.00;
+    theta = atan2(theta + ((inc_angular * 180.0)/(PI*RAIO_ROBO)));
+	//NumOut(20,LCD_LINE1,theta);
+}
+void atualiza_pose2(Pose &a){
+    atualiza_pose(a.x, a.y, a.theta);
+}
+float calc_dist(Pose atual,Pose ant){
+	float distancia;
+	distancia = sqrt(pow(atual.x - ant.x) + pow(atual.y - ant.y));
+	return distancia;
+}
+
+  
+float Co_ref(Pose pose_refer, float angulo){
+
+	Pose anterior;
+	anterior.x = 0;
+	anterior.y = 0;
+	anterior.theta = 0;
+	float hip_referencia;
+	float cat_oposto;
+	hip_referencia = calc_dist(pose_refer, anterior);
+	cat_oposto = (sin(angulo)*hip_referencia);
+	
+	return cat_oposto;
+}                                                                                                                              
+float Co_real(float co_ref, float sonar, float hipotenusa){
+	return asin((sonar - co_ref)/hipotenusa);
+}
+// angulo real do robô
+float robo_ang(float hip,float Co){
+	float angulo_rob;
+	if(hip!=0){
+		angulo_rob = Co/hip;
+		return asin(angulo_rob);
+	}
+	return 0;
+}
+#endif
